6f8d03cd475af11b2a9766f847225e90bcf5c9b7
Elona Bytyqi <bytyqielona9@gmail.com>
2023-12-08 09:37:44 +0100

    projekti pjesa pare

---
+++ #1.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded"),
+function(ngjarja) {
+const BtnSubmit = document.getElementById('submit-btn');
+}
+
+const validate = (ngjarja) => {
+ngjarja.preventDefault();}
+
+const perdoruesi = document.getElementById('userid');
+
+
+const fjalkalimi = document.getElementById('pass');
+
+
+const emrin = document.getElementById('emri');
+
+
+const emailin = document.getElementById('adresaEmailit');
+
+
+    if (perdoruesi.value === "") {
+alert("Ju lutem shtoni perdoruesin.");
+perdoruesi.focus();
+return false;
+}
+    if (fjalkalimi.value === "") {
+alert("Ju lutem shtoni Fjalkalimin.");
+fjalkalimi.focus();
+return false;
+}
+    if (emrin.value === "") {
+alert("Ju lutem shtoni emrin e Plote.");
+emrin.focus();
+return false;
+}
+    if (emailin.value === "") {
+alert("Ju lutem shtoni email'in.");
+emailin.focus();
+return false;
+    }
